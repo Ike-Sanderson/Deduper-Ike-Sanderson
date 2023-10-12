7be3633ce699c9412f8f11cf7f2e0841cc1ee960
Ike Sanderson <ikes@uoregon.edu>
2023-10-11 21:31:45 -0700

    homework

---
+++ deduper.py
@@ -0,0 +1,82 @@
+#!Usr/bin/env python
+
+
+
+
+'''The problem:
+PCR can amplify copies of some DNA molecules more than others. This inequality 
+can give an inaccurate picure of a cell's activity
+
+In addition, soft clipping can affect the accurate ID of the 5' starting position, 
+and if a read is from the reverse strand, a couple of extra steps must be taken to correct the soft clipping'''
+
+
+'''This solution A) corrects the POS, B) makes a dictionary with RNAME:POS:UMI and C) uses the dictionary 
+to direct which reads are writtent to the output SAM file'''
+
+import re
+
+RNAMEdict = {}
+
+'''Function'''
+#position_correct function
+def position_correct(initial_POS: int) -> int:
+
+    '''this function will iterate through reads and fix all starting left (5') positions, 
+        accounting for soft clip where needed. Write all lines (including any corrected POS) to a temp SAM file'''
+
+#open input SAM file as read, open tempSAM file as write
+    #for loop iterates through
+        #readline
+            #if line begins with "N":
+                #if forward strand, i.e. if((flag&16) != 16)
+                    #if soft clip, extract countback from cigar, adjust POS
+                    #if re.match('$6~/^(([1-9]|[1-9][0-9])[S]$/', readline) <- maybe this will work?
+                        #countback = the same regex used above
+                        #true_leftPOS = readline[$4] + countback
+                        #write readline with true_leftPOS into tempSAM file
+
+                #else, i.e. if((flag&16) == 16) --> strand is revcomp
+                    #if soft clip, extract countback from cigar, adjust POS
+                    #if re.match('$6~/^(([1-9]|[1-9][0-9])[S]$/', readline) <- maybe this will work?
+                        #countback = the same regex used above
+                        #true_leftPOS = readline[$4] - countback
+                        #write readline with true_leftPOS into tempSAM file
+            #else:
+                #write readline to tempSAM file
+    #close input SAM file, close tempSAM file    
+    
+
+'''Main'''
+
+'''iterate through temp SAM file and build dictionary of RNAME=key: POS = value
+ for each dict entry, write line to new SAM file
+ as build dict, check to see if key:value already exists, if so => do not write'''
+
+
+#call position_correct function
+
+#open tempSAM file as read, open output SAM file as write
+    #for loop line in tempSAM file
+        #readline
+            #if readline != ('^N') <- trying to say "does not start with N"
+                #write line to output SAM file --> Copying all the header lines into the new file
+            
+            #else 
+                #if RNAME, POS, and UMI already in RNAMEdict (i.e. readline[$3] RNAME==key and 
+                #                                      readline[4]==RNAMEdict 1st value and
+                #                                      readline[1]==RNAMEdict 2nd value)
+                    # continue i.e. skip, do NOT write from input to output
+                
+                #else 
+                    #RNAME = readline[$3]
+                    #RNAMEdict[RNAME] = [readline[$4]]  assign POS as 1st value for RNAME key, add QNAME (from $1) to 2nd value
+                    #write readline to output SAM file
+    #close input and output SAM files
+
+
+
+
+
+
+
---
+++ unit-test-folder/readme.md
@@ -0,0 +1,13 @@
+# Test file for deduper
+
+## cases covered in test.sam
+line 25 unique [will be written]
+line 26 unique [will be written]
+line 27 unique [will be written]
+line 28 2 base soft clip duplicate of line 25, adjusted POS +2 [will not be written]
+line 29 exact duplicate of line 25 [will not be written]
+line 30 not a duplicate; bitwise flag 16 tells us reverse complement [will be written]
+line 31 2 base soft clip duplicate of line 103 -strand, adjusted POS -2 [will not be written]
+insertion of leftmost bases, affecting POS, not yet tested in this file; not sure how to account for error that should be skipped
+UMI error, not sure how to capture this unless I accept all UMIs 
+deletion of leftmost bases, affecting POS, not yet tested in this file; not sure how to account for error that should be skipped
